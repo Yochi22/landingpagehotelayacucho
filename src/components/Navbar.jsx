daef2ac736074802d749e6a274cc833b9870d003
Yochi22 <116101499+Yochi22@users.noreply.github.com>
2023-05-06 16:23:23 -0600

    navbar ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 
 import React, { useState } from 'react';
+import logo from '../assets/logo.jpg'
+
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -11,30 +13,29 @@ const Navbar = () => {
   return (
   <>
   
-  <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-  <div class="flex items-center flex-shrink-0 text-white mr-6">
-    <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-    <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+  <nav className="flex items-center justify-between flex-wrap bg-white p-6 shadow-md">
+  <div className="flex items-center flex-shrink-0 text-amber-800 mr-6">
+    <img className='h-12' src={logo} alt="logo" />
   </div>
-  <div class="block lg:hidden">
-    <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white" onClick={toggleMenu}>
-      <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+  <div className="block lg:hidden">
+    <button className="flex items-center px-3 py-2 border rounded text-amber-800 border-amber-800 hover:text-amber-800 hover:border-amber-800" onClick={toggleMenu}>
+      <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
     </button>
   </div>
-  <div class={`w-full block lg:flex lg:items-center lg:w-auto ${isOpen ? '' : 'hidden'}`}>
-    <div class="text-sm lg:flex-grow">
-      <a href="#responsive-header" class="mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-        Inicio
+  <div className={`w-full block lg:flex lg:items-center lg:w-auto pr-14 ${isOpen ? '' : 'hidden'}`}>
+    <div className="text-sm lg:flex-grow">
+      <a href="#responsive-header" className="mt-4 lg:inline-block lg:mt-0 font-bold text-stone-500 hover:text-amber-800 mr-4">
+        INICIO
       </a>
-      <a href="#responsive-header" class="mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-        Nosotros
+      <a href="#responsive-header" className="mt-4 lg:inline-block lg:mt-0 font-bold text-stone-500 hover:text-amber-800 mr-4">
+        NOSOTROS
       </a>
-      <a href="#responsive-header" class=" mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-        Habitaciones
+      <a href="#responsive-header" className=" mt-4 lg:inline-block lg:mt-0 font-bold text-stone-500 hover:text-amber-800 mr-4">
+        HABITACIONES
       </a>
     
-      <a href="#responsive-header" class="mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
-        Contacto
+      <a href="#responsive-header" className="mt-4 lg:inline-block lg:mt-0 font-bold text-stone-500 hover:text-amber-800">
+        CONTACTO
       </a>
 
     </div>
